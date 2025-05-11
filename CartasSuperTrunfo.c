e80cb2ba6df20a3f193c4d16d5c799649863bb18
Daniel Teixeira Araújo <131397362+daniel-arjo@users.noreply.github.com>
2025-05-11 20:17:20 +0000

    Entrada e Exibição de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,50 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    // Variáveis primeira cidade
+    int pop1, pontos1;
+    float area1, pib1;
+    
+    // Variáveis segunda cidade
+    int pop2, pontos2;
+    float area2, pib2;
+
+    // Cadastro da primeira cidade
+    printf("Dados da primeira cidade:\n");
+    printf("Populacao: ");
+    scanf("%d", &pop1);
+    printf("Area (km²): ");
+    scanf("%f", &area1);
+    printf("PIB (milhoes): ");
+    scanf("%f", &pib1);
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontos1);
+
+    // Cadastro da segunda cidade
+    printf("\nDados da segunda cidade:\n");
+    printf("Populacao: ");
+    scanf("%d", &pop2);
+    printf("Area (km²): ");
+    scanf("%f", &area2);
+    printf("PIB (milhoes): ");
+    scanf("%f", &pib2);
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontos2);
+
+    // Mostrar os dados
+    printf("\nResumo:\n");
+    
+    printf("\nCidade 1:\n");
+    printf("Populacao: %d\n", pop1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f milhoes\n", pib1);
+    printf("Pontos turisticos: %d\n", pontos1);
+    
+    printf("\nCidade 2:\n");
+    printf("Populacao: %d\n", pop2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f milhoes\n", pib2);
+    printf("Pontos turisticos: %d\n", pontos2);
+
     return 0;
 }
